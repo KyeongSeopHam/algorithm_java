a315cb92934e8e5dd8b3f7c62c6c6af565ffd091
HamKyeongSeop <43702182+KyeongSeopHam@users.noreply.github.com>
2022-07-30 17:02:05 +0900

    [Silver III] Title: N과 M (3), Time: 640 ms, Memory: 208680 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,44 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 
 public class Main {
 
-    public static int[] arr;
-    public static int N, M;
-    public static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    static int N;  static int M;
+    static int[] arr;
+    static StringBuilder sb =new StringBuilder();
 
-        N = in.nextInt();
-        M = in.nextInt();
-        arr = new int[M];
+    public static void main(String[] args) throws IOException {
 
-        dfs(0);
-        System.out.print(sb);
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+
+        arr=new int[M];
+        DFSDFS(0);
+        System.out.println(sb);
+
 
     }
-//  1) 1 ~ N 까지의 수를 조합한다.
-//
-//  2) M 개를 선택하여 조합한다. (길이(깊이)가 M이다)
-//
-//  3) 중복해서 조합 할 수 있다.
-    public static void dfs(int depth) {
-  //N=3  M=2    1 1  1 2  1 3 ...-> 3 1 ... 3 3
 
+    private static void DFSDFS(int depth){
         if(depth==M){
-            for(int i=0; i<M; i++){
-                sb.append(arr[i] +" ");
-            }sb.append('\n');
+            for(int item:arr){
+                sb.append(item+" ");
+            }
+            sb.append("\n");
             return ;
         }
-        for(int i=1; i<=N; i++){
-            arr[depth]=i;
-            dfs(depth+1);
+
+        for(int i=0; i<N; i++){
+            arr[depth] = i+1;
+            DFSDFS(depth+1);
         }
 
-
     }
+
 }
-
-
