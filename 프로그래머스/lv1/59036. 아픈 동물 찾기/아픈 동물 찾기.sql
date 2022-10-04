b77caf18fc0d21f9b89d6c2864f17a49294b6d97
HamKyeongSeop <43702182+KyeongSeopHam@users.noreply.github.com>
2022-10-04 16:00:27 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/lv1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,3 +1,4 @@
-select animal_id, name from animal_ins
-where INTAKE_CONDITION = 'sick'
-order by animal_id 
+select animal_id, name
+from animal_ins
+where intake_condition='sick'
+order by animal_id
