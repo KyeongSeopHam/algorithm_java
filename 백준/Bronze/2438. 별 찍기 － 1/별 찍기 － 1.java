b6e82a0ceacd3ec7a1d4f036b96933b5eb2341a3
HamKyeongSeop <43702182+KyeongSeopHam@users.noreply.github.com>
2022-11-05 23:52:39 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -8,15 +8,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
         for(int i=1; i<=N; i++){
            for(int j=1; j<=i; j++){
-               System.out.print("*");
+               sb.append("*");
            }
-            System.out.println();
-
+            sb.append("\n");
         }
+        System.out.println(sb);
 
     }
 }
-
 
